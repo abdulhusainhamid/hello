6f4265e12b41d49ff96bb43aa7977737bef5dac5
abdulhusainhamid <97889309+abdulhusainhamid@users.noreply.github.com>
2022-01-27 22:07:00 +0530

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -4,6 +4,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world, universe, Asian, Indians, Gujarati, Marathi");
+        System.out.println("Hello world, universe, Asian, Indians, Gujarati, Marathi, Panjabi");
     }
 }
